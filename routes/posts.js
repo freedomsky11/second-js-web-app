776c302c4cbff1ac5364b70c467a7cca353db176
freedomsky <hanwei_1989@126.com>
2017-11-17 22:24:58 +0800

    segment9

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Get posts page. */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: '码字空间' });
+  res.render('posts', { title: '码字空间', postsList: ['文章1', '文章2', '文章3'] });
 });
 
 module.exports = router;
