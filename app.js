4f96961e1faeebd552aa1b977412d4d59f882e5d
freedomsky <hanwei_1989@126.com>
2017-11-29 17:25:16 +0800

    segment20

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('./models/init');
+var expressLayouts = require('express-ejs-layouts');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -14,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
